29288677120f0392f7e999c9c7426bae131f25c0
GSingh2432002 <gsingh332211@gmail.com>
2023-09-06 23:33:08 +0530

    Update

---
+++ Qs11.js
@@ -0,0 +1,8 @@
+// Define a function called greet that takes a name parameter
+function greet(name) {
+    // Use a console.log statement to output a greeting message with the name parameter
+    console.log(`Hello ${name}! How are you doing today?`);
+  }
+  
+  // Call the greet function and pass in the name "Gaurav" as an argument
+  greet("Gaurav"); 
